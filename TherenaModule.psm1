a61f8bc36cc2a8f3ed84daa5352e1317fab81ea4
David Roller <david-roller@outlook.com>
2018-10-23 22:01:44 +0200

    Added the Get-OperatingSystemBitness and Get-DebuggerPath function

--- TherenaModule.psm1
+++ TherenaModule.psm1
@@ -1,4 +1,63 @@
+<#
+
+.SYNOPSIS
+
+Powershell module with helper function for my daily software development
+
+.DESCRIPTION
+
+All the tooling which is needed for my daily software develpment work.
+There is everything from debugging, dump analysis to coding etc.
+
+.LINK
+
+https://github.com/Therena/PowerShellTools
+
+.LICENSE
+
+Copyright 2018 David Roller 
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+#>
+
+
 function Get-DebuggerPath {
+<#
+
+.SYNOPSIS
+Get the paths to the Windows Debug (WinDBG) executables in the installed Windows kits 
+
+.DESCRIPTION
+This function searches for the Windows Debug executable files in the installed windows kit (SDK, WDK).
+Please install at least one Windows kit (SDK, WDK) version before using this function.
+
+.LINK
+
+https://github.com/Therena/PowerShellTools
+https://developer.microsoft.com/en-us/windows/downloads/windows-10-sdk
+https://docs.microsoft.com/en-us/windows-hardware/drivers/download-the-wdk
+
+.EXAMPLE
+Get-DebuggerPath
+
+Path                                                            WDK Bitness
+----                                                            --- -------
+C:\Program Files (x86)\Windows Kits\10\Debuggers\arm\windbg.exe 10  arm    
+C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\windbg.exe 10  x64    
+C:\Program Files (x86)\Windows Kits\10\Debuggers\x86\windbg.exe 10  x86   
+
+#>
     PROCESS {
         $windbgList = New-Object System.Collections.ArrayList
         $files = Get-ChildItem -Path 'C:\Program Files (x86)\Windows Kits' -Filter 'windbg.exe' -File -Recurse -ErrorAction SilentlyContinue -Force
@@ -16,6 +75,26 @@ function Get-DebuggerPath {
 }
 
 function Get-OperatingSystemBitness {
+<#
+
+.SYNOPSIS
+Get bitness of the installed Windows operating system
+
+.DESCRIPTION
+This function optains the bitness of the current installed Microsoft Windows operating system
+
+.LINK
+
+https://github.com/Therena/PowerShellTools
+
+.EXAMPLE
+Get-OperatingSystemBitness
+
+Type
+----
+x64  
+
+#>
     PROCESS {
         if ([Environment]::Is64BitProcess -ne [Environment]::Is64BitOperatingSystem) {
             $obj = new-object psobject -Property @{ Type = 'x86' }
@@ -26,7 +105,23 @@ function Get-OperatingSystemBitness {
     }
 }
 
-function Connect-Debugger {
+function Connect-KernelDebugger {
+<#
+
+.SYNOPSIS
+
+
+.DESCRIPTION
+
+
+.LINK
+
+https://github.com/Therena/PowerShellTools
+
+.EXAMPLE
+Connect-KernelDebugger
+
+#>
     [CmdletBinding()]
     param (
         [parameter(Mandatory=$true)]
@@ -44,4 +139,4 @@ function Connect-Debugger {
 
 Export-ModuleMember -Function Get-OperatingSystemBitness
 Export-ModuleMember -Function Get-DebuggerPath
-Export-ModuleMember -Function Connect-Debugger
+Export-ModuleMember -Function Connect-KernelDebugger
