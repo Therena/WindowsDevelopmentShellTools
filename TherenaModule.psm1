af1a17c490acc388383743c194bfbe0abfa7ed99
Therena <18515874+Therena@users.noreply.github.com>
2018-10-24 12:43:15 +0200

    Finalize the Get-DumpAnalysis function

--- TherenaModule.psm1
+++ TherenaModule.psm1
@@ -194,13 +194,52 @@ Runs and prints an analysis of a crash dump file
 
 .DESCRIPTION
 
+Forwards the give the crash dump file to the installed kernel debugger to get otain some details about the issue.
 
 .LINK
 
 https://github.com/Therena/PowerShellTools
 
 .EXAMPLE
-Get-DumpAnalysis
+Get-DumpAnalysis -File "dwm.exe.1168.dmp"
+
+Microsoft (R) Windows Debugger Version 10.0.17134.1 AMD64
+Copyright (c) Microsoft Corporation. All rights reserved.
+
+
+Loading Dump File [dwm.exe.1168.dmp]
+User Mini Dump File with Full Memory: Only application data is available
+
+
+************* Path validation summary **************
+Response                         Time (ms)     Location
+Deferred                                       srv*http://msdl.microsoft.com/download/symbols
+Executable search path is: 
+Windows 10 Version 16299 MP (4 procs) Free x86 compatible
+Product: WinNt, suite: SingleUserTS
+16299.15.x86fre.rs3_release.170928-1534
+Machine Name:
+Debug session time: Wed Oct 24 05:51:28.000 2018 (UTC + 2:00)
+System Uptime: 0 days 0:26:30.274
+Process Uptime: 0 days 0:26:16.000
+.......................................................
+Loading unloaded module list
+...........................................
+This dump file has an exception of interest stored in it.
+The stored exception information can be accessed via .ecxr.
+(490.530): Unknown exception - code 8898008d (first/second chance not available)
+eax=03e5f270 ebx=00000000 ecx=00000000 edx=00000000 esi=03e5f580 edi=03e5f270
+eip=725fb99e esp=03e5f554 ebp=03e5f5dc iopl=0         nv up ei pl nz ac po nc
+cs=001b  ss=0023  ds=0023  es=0023  fs=003b  gs=0000             efl=00200212
+dwmcore!CComposition::ProcessComposition+0xa0830:
+725fb99e e9fcf8f5ff      jmp     dwmcore!CComposition::ProcessComposition+0x131 (7255b29f)
+0:002> kd: Reading initial command '!analyze -v;~* k;q'
+*******************************************************************************
+*                                                                             *
+*                        Exception Analysis                                   *
+*                                                                             *
+*******************************************************************************
+...
 
 #>
     [CmdletBinding()]
