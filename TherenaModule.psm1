1a582b5faee7fe2c15c8850d73f8fdbd49cc2b50
Therena <18515874+Therena@users.noreply.github.com>
2018-11-04 21:25:18 +0100

    Added function Get-FileDetails

--- TherenaModule.psm1
+++ TherenaModule.psm1
@@ -717,6 +717,135 @@ SYMCHK: PASSED + IGNORED files = 1
     }
 }
 
+function Get-FileDetails {
+<#
+
+.SYNOPSIS
+Optain the details of the given file(s) or directory
+
+.DESCRIPTION
+Optain details about the given file(s) or directories. 
+Details like Version numbers, file descriptions, company, etc. will be read from the file.
+
+.PARAMETER File
+File(s) or directory for which the details should be obtained 
+
+.PARAMETER Filter
+Filter for specific file types
+
+.PARAMETER Recursive
+Defines if the the sub directories should be taken into account as well
+
+.EXAMPLE
+Get-FileDetails C:\Windows\regedit.exe
+
+
+File            : C:\Windows\regedit.exe
+FileVersion     : 10.0.17134.346 (WinBuild.160101.0800)
+ProductVersion  : 10.0.17134.346
+FileDescription : Registrierungs-Editor
+CompanyName     : Microsoft Corporation
+InternalName    : REGEDIT
+LegalCopyright  : © Microsoft Corporation. Alle Rechte vorbehalten.
+ProductName     : Betriebssystem Microsoft® Windows®
+
+.EXAMPLE
+Get-FileDetails C:\Windows | Format-Table
+
+File                            FileVersion                           ProductVersion FileDescription                                       CompanyName                InternalName   
+----                            -----------                           -------------- ---------------                                       -----------                ------------   
+C:\Windows\bfsvc.exe            10.0.17134.346 (WinBuild.160101.0800) 10.0.17134.346 Startdatei-Wartungshilfsprogramm                      Microsoft Corporation      bfsvc.exe      
+C:\Windows\bootstat.dat                                                                                                                                                              
+C:\Windows\comsetup.log                                                                                                                                                              
+C:\Windows\diagerr.xml                                                                                                                                                               
+C:\Windows\diagwrn.xml                                                                                                                                                               
+C:\Windows\DtcInstall.log                                                                                                                                                            
+C:\Windows\explorer.exe         10.0.17134.346 (WinBuild.160101.0800) 10.0.17134.346 Windows-Explorer                                      Microsoft Corporation      explorer       
+C:\Windows\HelpPane.exe         10.0.17134.346 (WinBuild.160101.0800) 10.0.17134.346 Microsoft-Hilfe und Support                           Microsoft Corporation      HelpPane.exe   
+C:\Windows\hh.exe               10.0.17134.346 (WinBuild.160101.0800) 10.0.17134.346 Ausführbare Microsoft®-HTML-Hilfsdatei                Microsoft Corporation      HH 1.41        
+C:\Windows\mib.bin                                                                                                                                                                   
+C:\Windows\notepad.exe          10.0.17134.346 (WinBuild.160101.0800) 10.0.17134.346 Editor                                                Microsoft Corporation      Notepad        
+C:\Windows\PFRO.log                                                                                                                                                                  
+C:\Windows\Professional.xml                                                                                                                                                          
+C:\Windows\py.exe               3.6.6                                 3.6.6          Python                                                Python Software Foundation Python Launcher
+C:\Windows\pyshellext.amd64.dll 3.6.6                                 3.6.6          Python                                                Python Software Foundation Python Launc...
+C:\Windows\pyw.exe              3.6.6                                 3.6.6          Python                                                Python Software Foundation Python Launcher
+C:\Windows\regedit.exe          10.0.17134.346 (WinBuild.160101.0800) 10.0.17134.346 Registrierungs-Editor                                 Microsoft Corporation      REGEDIT        
+C:\Windows\setupact.log                                                                                                                                                              
+C:\Windows\setuperr.log                                                                                                                                                              
+C:\Windows\splwow64.exe         10.0.17134.1 (WinBuild.160101.0800)   10.0.17134.1   Print driver host for applications                    Microsoft Corporation      splwow64.exe   
+C:\Windows\system.ini                                                                                                                                                                
+C:\Windows\twain_32.dll         1,7,1,3                               1,7,1,0        Twain_32 Source-Manager (Image Acquisition Interface) Twain Working Group        DSM            
+C:\Windows\win.ini                                                                                                                                                                   
+C:\Windows\WindowsUpdate.log                                                                                                                                                         
+C:\Windows\winhlp32.exe         10.0.17134.1 (WinBuild.160101.0800)   10.0.17134.1   Windows Winhlp32-Stub                                 Microsoft Corporation      WINHSTB        
+C:\Windows\WMSysPr9.prx                                                                                                                                                              
+C:\Windows\write.exe            10.0.17134.1 (WinBuild.160101.0800)   10.0.17134.1   Windows Write                                         Microsoft Corporation      write  
+
+.EXAMPLE
+Get-FileDetails C:\Windows -Filter *.exe | Format-Table
+
+File                    FileVersion                           ProductVersion FileDescription                        CompanyName                InternalName    LegalCopyright        
+----                    -----------                           -------------- ---------------                        -----------                ------------    --------------        
+C:\Windows\bfsvc.exe    10.0.17134.346 (WinBuild.160101.0800) 10.0.17134.346 Startdatei-Wartungshilfsprogramm       Microsoft Corporation      bfsvc.exe       © Microsoft Corpora...
+C:\Windows\explorer.exe 10.0.17134.346 (WinBuild.160101.0800) 10.0.17134.346 Windows-Explorer                       Microsoft Corporation      explorer        © Microsoft Corpora...
+C:\Windows\HelpPane.exe 10.0.17134.346 (WinBuild.160101.0800) 10.0.17134.346 Microsoft-Hilfe und Support            Microsoft Corporation      HelpPane.exe    © Microsoft Corpora...
+C:\Windows\hh.exe       10.0.17134.346 (WinBuild.160101.0800) 10.0.17134.346 Ausführbare Microsoft®-HTML-Hilfsdatei Microsoft Corporation      HH 1.41         © Microsoft Corpora...
+C:\Windows\notepad.exe  10.0.17134.346 (WinBuild.160101.0800) 10.0.17134.346 Editor                                 Microsoft Corporation      Notepad         © Microsoft Corpora...
+C:\Windows\py.exe       3.6.6                                 3.6.6          Python                                 Python Software Foundation Python Launcher Copyright © 2001-20...
+C:\Windows\pyw.exe      3.6.6                                 3.6.6          Python                                 Python Software Foundation Python Launcher Copyright © 2001-20...
+C:\Windows\regedit.exe  10.0.17134.346 (WinBuild.160101.0800) 10.0.17134.346 Registrierungs-Editor                  Microsoft Corporation      REGEDIT         © Microsoft Corpora...
+C:\Windows\splwow64.exe 10.0.17134.1 (WinBuild.160101.0800)   10.0.17134.1   Print driver host for applications     Microsoft Corporation      splwow64.exe    © Microsoft Corpora...
+C:\Windows\winhlp32.exe 10.0.17134.1 (WinBuild.160101.0800)   10.0.17134.1   Windows Winhlp32-Stub                  Microsoft Corporation      WINHSTB         © Microsoft Corpora...
+C:\Windows\write.exe    10.0.17134.1 (WinBuild.160101.0800)   10.0.17134.1   Windows Write                          Microsoft Corporation      write           © Microsoft Corpora...
+
+#>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$true)]
+        [string]$File,
+
+        [string]$Filter = "*.*",
+        
+        [switch]$Recursive    
+    )
+    
+    $Table = New-Object System.Data.DataTable "File Details"
+    $Table.Columns.Add($(New-Object system.Data.DataColumn File, ([string])))
+    $Table.Columns.Add($(New-Object system.Data.DataColumn FileVersion, ([string])))
+    $Table.Columns.Add($(New-Object system.Data.DataColumn ProductVersion, ([string])))
+    $Table.Columns.Add($(New-Object system.Data.DataColumn FileDescription, ([string])))
+    $Table.Columns.Add($(New-Object system.Data.DataColumn CompanyName, ([string])))
+    $Table.Columns.Add($(New-Object system.Data.DataColumn InternalName, ([string])))
+    $Table.Columns.Add($(New-Object system.Data.DataColumn LegalCopyright, ([string])))
+    $Table.Columns.Add($(New-Object system.Data.DataColumn ProductName, ([string])))
+     
+    if ($Recursive) {
+        $FoundFiles = Get-ChildItem -Path $File -File -Filter $Filter -Recurse
+    } else {
+        $FoundFiles = Get-ChildItem -Path $File -File -Filter $Filter
+    }
+
+    foreach($FileEntry in $FoundFiles) {
+        $VersionInfo = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($FileEntry.FullName)
+
+        $Row = $Table.NewRow()
+    
+        $Row.File = $FileEntry.FullName
+        $Row.FileVersion = $VersionInfo.FileVersion
+        $Row.ProductVersion = $VersionInfo.ProductVersion
+        $Row.FileDescription = $VersionInfo.FileDescription
+        $Row.CompanyName = $VersionInfo.CompanyName
+        $Row.InternalName = $VersionInfo.InternalName
+        $Row.LegalCopyright = $VersionInfo.LegalCopyright
+        $Row.ProductName = $VersionInfo.ProductName
+
+        $Table.Rows.Add($Row)
+    }
+
+    return $Table
+}
+
 #
 # Export the members of the module
 #
@@ -731,3 +860,4 @@ Export-ModuleMember -Function Get-LinesOfCode
 Export-ModuleMember -Function Get-EicarSignature
 Export-ModuleMember -Function Get-SymbolCheck
 Export-ModuleMember -Function Find-Symbols
+Export-ModuleMember -Function Get-FileDetails
